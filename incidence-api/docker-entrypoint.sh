863d3a7e8170faa3ff6bc1d76fa147881dc390b6
Edwin Cruz <edc7012@cumc.columbia.edu>
2023-05-26 14:53:19 +0000

    Enhanced entry script to check configured datasource is up before starting server.

--- incidence-api/docker-entrypoint.sh
+++ incidence-api/docker-entrypoint.sh
@@ -1,26 +1,18 @@
 #!/bin/sh
+DATABASESERVER_CHECK_MAX_ATTEMPTS=10
+DATABASESERVER_CHECK_SLEEP_TIME=30
 
 echo "Application Running"
 
-echo ""
-echo "ENVIRONNMENT VARIABLES"
-echo "------------------------------------------"
-echo "DATASOURCE_USERNAME: "$DATASOURCE_USERNAME
-echo "DATASOURCE_HOSTNAME: "$DATASOURCE_HOSTNAME
-echo "DATASOURCE_PORT: "$DATASOURCE_PORT
-echo "DATASOURCE_SCHEMA: "$DATASOURCE_SCHEMA
-echo "DATASOURCE_DATABASE: "$DATASOURCE_DATABASE
-echo "------------------------------------------"
-
 source venv/bin/activate
 
-python2 --version
-echo ""
-echo "which: " $(which python2)
 echo ""
 echo "PATH: "$PATH
 echo ""
+
+python2 --version
 str=$(which python2)
+echo "PYTHONEXE:"$str
 
 oldstr="\/bin\/python2"
 newstr=""
@@ -36,7 +28,57 @@ export PYTHONPATH=$pythonpath
 echo ""
 
 python -m site
+echo ""
+
+ping_database_server() {
+  attempt=1  
+  while [ "$attempt" -le "$DATABASESERVER_CHECK_MAX_ATTEMPTS" ]
+  do
+    # Ping the server. Change 1 to the number of packets you want to send.
+    ping -c 1 "$DATASOURCE_HOSTNAME" >/dev/null 2>&1
+
+    # If the server is reachable
+    if [ "$?" -eq 0 ]
+    then
+      echo "DATASOURCE CHECK: Server is up. Starting API server..."
+      echo ""
+      return 0
+    else
+      echo "DATASOURCE CHECK: Server is not reachable. Retry after $DATABASESERVER_CHECK_SLEEP_TIME seconds. Attempt #$attempt of $DATABASESERVER_CHECK_MAX_ATTEMPTS."
+      
+      # Increment attempt counter
+      attempt=$(expr $attempt + 1)
+
+      # If maximum attempts reached
+      if [ $attempt -gt $DATABASESERVER_CHECK_MAX_ATTEMPTS ]
+      then
+        echo "Max attempts reached. Exiting..."
+        return 1
+      else
+        # Wait before retrying
+        sleep $DATABASESERVER_CHECK_SLEEP_TIME
+
+      fi
+    fi
+  done
+}
+
+print_datasource_info(){
+  echo ""
+  echo "ENVIRONNMENT VARIABLES"
+  echo "------------------------------------------"
+  echo "DATASOURCE_USERNAME: "$DATASOURCE_USERNAME
+  echo "DATASOURCE_HOSTNAME: "$DATASOURCE_HOSTNAME
+  echo "DATASOURCE_PORT: "$DATASOURCE_PORT
+  echo "DATASOURCE_SCHEMA: "$DATASOURCE_SCHEMA
+  echo "DATASOURCE_DATABASE: "$DATASOURCE_DATABASE
+  echo "------------------------------------------"
+}
+print_datasource_info
+
+ping_database_server
 
 python2 ./incidence.py
 
+echo ""
 echo "Application Stopped"
