042caab7f272245a582ba9050fce46af58ae8aa8
Taha Abdul-Basser <ta2471@cumc.columbia.edu>
2023-06-02 11:54:06 -0400

    Feature/dbmi cicd (#30)
    * Fix error in Jenkins file
    
    * Enhance Jenkile file to change to target directory, pull test repository, checkout feature branch and startup application services
    
    * Fixed curly braces in Jenkins file
    
    * Expose traefik dashboard, suppress container restart
    
    * Modify traefik API settings
    Expose Traafik API on 8080
    Stub out SSH script in Jenkins
    
    * * Change CRLF --> LF to fix error message 'exec user process caused: no such file or directory'
    * remove database ping to address ping permission error
    
    * *Change name of traefik container to avoid name collision on deployment server
    * Comment in server dependencies
    
    * * Remove unused environment variable
    * Comment in healthcheck block
    * Remove whitespaces
    
    * Remove healthcheck because it causes the backend routing to fail
    
    * Modify configuration to expose application at a port that will not collide with active HTTP port
    
    * Remove unused environment varaible
    
    * Removed reference to unused environment variable
    
    * Change default constant
    
    * Modify proxy routing to support 127.0.0.1 or configurable host name.
    
    * Add POC version Gitlab CI/CD pipeline

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,28 +2,28 @@ pipeline {
   agent any
 
   stages {
-    stage ('Build') {
-      steps {
-        echo 'Building..'
-        #sh 'docker-compose build .'
-      }
-    }
+  //   stage ('Build') {
+  //     steps {
+  //       echo 'Building..'
+  //       //sh 'docker-compose build .'
+  //     }
+  //   }
 
-    stage ('Archive') {
-      steps {
-        echo 'Archiving..'
-        #sh 'docker push my-image'
-      }
-    }
+    // stage ('Archive') {
+    //   steps {
+    //     echo 'Archiving..'
+    //     //sh 'docker push my-image'
+    //   }
+    // }
 
     stage ('Deploy') {
       steps {
         echo 'Deploying..'
-        #ssh "${USERNAME}@${MACHINE_NAME}" "
-        #  cd /opt/ohdsi/howoften && git fetch --all && git checkout master && git pull && sudo docker-compose up -d
-        #"
-        #sh 'cd /opt/ohdsi/howoften && git fetch --all && git checkout feature/dbmi-cicd && git pull && sudo docker-compose up -d'
-        sh 'pwd'
+        ssh "${USERNAME}@${HOSTNAME}" "
+          cd ~/drug-condition-incidence
+          git checkout feature/dbmi-cicd
+          docker-compose up -d
+        "
       }
     }
   }
--- incidence-api/docker-entrypoint.sh
+++ incidence-api/docker-entrypoint.sh
@@ -30,39 +30,6 @@ echo ""
 python -m site
 echo ""
 
-ping_database_server() {
-  attempt=1  
-  while [ "$attempt" -le "$DATABASESERVER_CHECK_MAX_ATTEMPTS" ]
-  do
-    # Ping the server. Change 1 to the number of packets you want to send.
-    ping -c 1 "$DATASOURCE_HOSTNAME" >/dev/null 2>&1
-
-    # If the server is reachable
-    if [ "$?" -eq 0 ]
-    then
-      echo "DATASOURCE CHECK: Server is up.."
-      echo ""
-      return 0
-    else
-      echo "DATASOURCE CHECK: Server is not reachable. Retry after $DATABASESERVER_CHECK_SLEEP_TIME seconds. Attempt #$attempt of $DATABASESERVER_CHECK_MAX_ATTEMPTS."
-      
-      # Increment attempt counter
-      attempt=$(expr $attempt + 1)
-
-      # If maximum attempts reached
-      if [ $attempt -gt $DATABASESERVER_CHECK_MAX_ATTEMPTS ]
-      then
-        echo "Max attempts reached. Exiting..."
-        return 1
-      else
-        # Wait before retrying
-        sleep $DATABASESERVER_CHECK_SLEEP_TIME
-
-      fi
-    fi
-  done
-}
-
 print_datasource_info(){
   echo ""
   echo "ENVIRONNMENT VARIABLES"
@@ -75,8 +42,6 @@ print_datasource_info(){
   echo "------------------------------------------"
 }
 print_datasource_info
-
-ping_database_server
 
 python ./incidence.py
 
