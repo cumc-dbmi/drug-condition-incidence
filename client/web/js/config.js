9c5b3e847fb5b57fb28bd9f8c8a4f5aef41c5455
Edwin Cruz <edc7012@cumc.columbia.edu>
2023-07-20 05:18:22 -0400

    Minor path correction for Traefik implementation.

--- client/web/js/config.js
+++ client/web/js/config.js
@@ -1,6 +1,6 @@
 angular.module('config', [])
 
-.constant('ApiBaseUrl', 'http://localhost:8080')
+.constant('ApiBaseUrl', 'http://127.0.0.1/incidence-api')
 
 .constant('VocabBaseUrl', 'http://api.ohdsi.org/WebAPI/vocabulary/1PCT')
 
