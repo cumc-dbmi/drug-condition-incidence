81edbfb3518dbef4acd85a758667e07eb0044091
Edwin Cruz <ecruz165@yahoo.com>
2023-09-18 16:07:06 -0400

    Work in progress for drilling into patients at risk by Country.

--- frontend-modern/src/app/_components/DrugConditionDetailsStackedBarChart.tsx
+++ frontend-modern/src/app/_components/DrugConditionDetailsStackedBarChart.tsx
@@ -1,14 +1,49 @@
 "use client"
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import HighchartsReact from 'highcharts-react-official';
 import Highcharts from 'highcharts';
+
+
+function removeCategoryAndData(chart, categoryName) {
+    const {categories} = chart.xAxis[0]
+    const categoryIndex = categories.indexOf(categoryName)
+    const {data} = chart.series[0]
+    // Filter data and categories
+    const filteredData = data.map((p) => p.y).filter((v, i) => i !== categoryIndex)
+    const filterdCategories = categories.filter((v, i) => i !== categoryIndex)
+
+    // Update chart with filtered data and categories
+    chart.update({xAxis: {categories: filterdCategories}, series: {data: filteredData}})
+}
 
 export const DrugConditionDetailsStackedBarChart = () => {
     const [hoverData, setHoverData] = useState(null);
     const [chartOptions, setChartOptions] = useState({
         chart: {
-            type: 'column'
+            type: 'column',
+            events: {
+                load: function() {
+                    var chart = this;
+                    var axis = this.xAxis[0]
+                    var ticks = axis.ticks
+                    var points = this.series[0].points
+                    var tooltip = this.tooltip
+
+                    points.forEach(function(point, i) {
+                        if (ticks[i]) {
+                            var label = ticks[i].label.element
+
+                            label.onclick = function() {
+                           //     alert('clicked on ' + point.category)
+                                removeCategoryAndData(chart, point.category)
+//                                tooltip.getPosition(null, null, point)
+  //                              tooltip.refresh(point)
+                            }
+                        }
+
+                    })
+                }}
         },
         title: {
             text: 'Patients at Risk by Country',
@@ -28,11 +63,11 @@ export const DrugConditionDetailsStackedBarChart = () => {
         },
         series: [{
             name: 'Required Full Time at Risk',
-            data: [729+1331540+22689+149164+526122+1331540+1915119, 206747, 4437, 10315,3004]
+            data: [729 + 1331540 + 22689 + 149164 + 526122 + 1331540 + 1915119, 206747, 4437, 10315, 3004]
         }, {
             name: 'Out Patient',
-            data: [2994+565972+32181+225403+666331+1832829+2693984, 260201, 5272, 22203, 5212]
-        }],    tooltip: {
+            data: [2994 + 565972 + 32181 + 225403 + 666331 + 1832829 + 2693984, 260201, 5272, 22203, 5212]
+        }], tooltip: {
             headerFormat: '<b>{point.x}</b><br/>',
             pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
         },
