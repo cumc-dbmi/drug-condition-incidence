74363a9858f1629db36216f66d53f3642c7518f1
Edwin Cruz <ecruz165@yahoo.com>
2023-09-26 03:00:44 -0400

    Updated mocks

--- frontend-modern/src/mocks/index.js
+++ frontend-modern/src/mocks/index.js
@@ -1,11 +1,11 @@
-const drugs = require('./drugs.json');
-const drugConditions = require('./drug-conditions-974166.json');
-const drugConditionDetail = require('./drug-conditions-974144-details-44784217.json');
-const demo = require('./demo.json');
+var drugs = require('./drugs.json');
+var drugConditions = require( './drug-conditions-974166.json');
+var drugConditionDetail = require ('./drug-conditions-974144-details-44784217.json');
 
-module.exports = () => ({
-  drugs: drugs,
-  'drug-conditions': drugConditions,
-  'drug-condition-details': drugConditionDetail,
-  demo: demo,
-});
+module.exports = () => {
+    return {
+        drugs,
+        "drug-conditions-974166": drugConditions,
+        "drug-conditions-974144-details-44784217": drugConditionDetail
+    };
+};
