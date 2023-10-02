d55e49f097e8496d16c3f0d47f018bca29584e0d
Edwin Cruz <ecruz165@yahoo.com>
2023-10-02 03:26:04 -0400

    Environment file config setup cleanup.

--- frontend-modern/src/app/_services/services.ts
+++ frontend-modern/src/app/_services/services.ts
@@ -5,9 +5,9 @@ import { QueryFunctionContext, QueryKey } from 'react-query';
 const backApiBaseUrl = process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL;
 
 async function fetchDrugs(): Promise<Drug[]> {
-  console.log(`fetchDrugs: ${backApiBaseUrl}/drugs`);
-  const res = await fetch(`${backApiBaseUrl}/drugs`);
-  console.log('fetchDrugs');
+  const url = `${backApiBaseUrl}/drugs`;
+  console.log(`fetchDrugs: ${url}`);
+  const res = await fetch(`${url}`);
   return res.json();
 }
 
@@ -15,7 +15,9 @@ async function fetchDrugConditionsById(
   drugId: number,
   signal: RequestInit | undefined
 ): Promise<DrugCondition[]> {
-  const res = await fetch(`${backApiBaseUrl}/drug-conditions`, signal);
+  const url = `${backApiBaseUrl}/drug-conditions/${drugId}`;
+  console.log(`fetchDrugConditionsById: ${url}`);
+  const res = await fetch(`${url}`, signal);
   return res.json();
 }
 
@@ -23,14 +25,16 @@ export const getDrugListAsListBoxItems = async (): Promise<
   AutosuggestListboxItem[]
 > => {
   const input: Drug[] = await fetchDrugList();
-
   return input.map((item) => ({
     label: item.drug_concept_name,
     value: item.drug_concept_id,
   }));
 };
+
 export const fetchDrugList = async (): Promise<Drug[]> => {
-  const response = await axios.get(`${backApiBaseUrl}/drugs`);
+  const url = `${backApiBaseUrl}/drugs`;
+  console.log(`fetchDrugList: ${url}`);
+  const response = await axios.get(`${url}`);
   return response.data;
 };
 
@@ -38,11 +42,9 @@ export async function getDrugConditionAsListBoxItems(
   context: QueryFunctionContext<QueryKey>
 ): Promise<AutosuggestListboxItem[]> {
   const [type, drugConceptId] = context.queryKey;
-
   if (type !== 'drugsConditions' || typeof drugConceptId !== 'number') {
     throw new Error('Invalid query key');
   }
-
   const input: DrugCondition[] = await fetchDrugConditionList(drugConceptId);
   return input.map((item) => ({
     label: item.outcome_concept_name,
@@ -51,11 +53,9 @@ export async function getDrugConditionAsListBoxItems(
 }
 
 export const fetchDrugConditionList = async (drugConceptId: number) => {
-  console.log('fetchDrugConditionList using drug concept id: ' + drugConceptId);
-  const response = await axios.get(
-    `${backApiBaseUrl}/drug-conditions/${drugConceptId}`
-  );
-  console.log(`${process.env.NEXT_PUBLIC_TEST}`);
+  const url = `${backApiBaseUrl}/drug-conditions/${drugConceptId}`;
+  console.log(`fetchDrugConditionList: ${url}`);
+  const response = await axios.get(`${url}`);
   return response.data;
 };
 
@@ -63,14 +63,8 @@ export const fetchDrugConditionDetailList = async (
   drugConceptId: number,
   outcomeConceptId: number
 ) => {
-  console.log(
-    'fetchDrugConditionDetailList using drug concept id: ' +
-      drugConceptId +
-      ' and outcome concept id: ' +
-      outcomeConceptId
-  );
-  const response = await axios.get(
-    `${backApiBaseUrl}/exposure-outcomes/${drugConceptId}/rates-and-sources/${outcomeConceptId}/365`
-  );
+  const url = `${backApiBaseUrl}/exposure-outcomes/${drugConceptId}/rates-and-sources/${outcomeConceptId}/365`;
+  console.log(`fetchDrugConditionDetailList: ${url}`);
+  const response = await axios.get(`${url}`);
   return response.data;
 };
